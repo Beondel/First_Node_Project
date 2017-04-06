964b9b7ad9f8e7855d833ff2cfc436a326284c0f
Beondel <bengmacmillan@gmail.com>
2017-04-05 23:43:11 -0700

    settimeout trial

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('bacon');
+function placeAnOrder(orderNumber) {
+    console.log("Customer order: " + orderNumber);
+    cookAndDeliverFood(function () {
+        console.log("Delivered food order:" + orderNumber);
+    });
+}
+
+//Simulate a 5 second operation
+function cookAndDeliverFood(callback) {
+    setTimeout(callback, 5000);
+}
+
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
+placeAnOrder(4);
+placeAnOrder(5);
+placeAnOrder(6);
